020ea1745e5c19b8229a7a0a1a17f62971692848
Patoke <patokekawaii@gmail.com>
2023-07-09 10:03:47 -0400

    ~ added some quality of life changes

--- relocs_dumper/src/includes.hpp
+++ relocs_dumper/src/includes.hpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <filesystem>
 
 #include <spdlog/spdlog.h>
 #include <safetyhook/safetyhook.hpp>
--- relocs_dumper/src/main.cpp
+++ relocs_dumper/src/main.cpp
@@ -15,7 +15,7 @@ int main( int argc, char *argv[] )
 
     dump1.open( dump_path1, std::ios::binary | std::ios::in );
     dump2.open( dump_path2, std::ios::binary | std::ios::in );
-    out_file.open( "relocs.hpp", std::ios::out | std::ios::in | std::ios::trunc );
+    out_file.open( "relocs.hpp", std::ios::out | std::ios::trunc );
 
     if ( dump1.fail( ) ) {
         spdlog::error( "failed to open dump 1" );
@@ -23,28 +23,37 @@ int main( int argc, char *argv[] )
     } else if ( dump2.fail( ) ) {
         spdlog::error( "failed to open dump 2" );
         return 0;
+    } else if ( out_file.fail( ) ) {
+        spdlog::error( "failed to open output file" );
+        return 0;
     }
 
-    if ( out_file.fail( ) ) {
-        spdlog::error( "failed to open dump 2" );
+    // check if both files have the same size
+    std::filesystem::path pdump1( dump_path1 ), pdump2( dump_path2 );
+    if ( std::filesystem::file_size( pdump1 ) != std::filesystem::file_size( pdump2 ) ) {
+        spdlog::error( "files are not the same size" );
         return 0;
     }
 
     std::istreambuf_iterator< char > dump1_it( dump1 ), dump2_it( dump2 ), end;
     std::vector< size_t > reloc_data;
+    std::stringstream out_str;
 
-    size_t curr_off{ 0x0 };
+    reloc_data.reserve( size_t(25) << 20 ); // reserve 25MB so we don't do extra allocs lel
 
     spdlog::info( "finding relocs..." );
 
     // iterate thru bytes
-    for ( ; dump1_it != end && dump2_it != end; ++dump1_it, ++dump2_it, curr_off++ ) {
+    for ( size_t curr_off{ 0x0 }; dump1_it != end && dump2_it != end; ++dump1_it, ++dump2_it, curr_off++ ) {
         if ( *dump1_it != *dump2_it )
             reloc_data.push_back( curr_off );
     }
 
+    // alr used these
+    dump1.close( );
+    dump2.close( );
+
     // setup format
-    std::stringstream out_str;
     out_str << "#pragma once\n"
             << "\n"
             << "std::array< size_t, " << reloc_data.size( ) << " > relocations = {";
@@ -69,8 +78,6 @@ static void fix_relocations(size_t dump1_base, size_t dump2_base) {
 
     out_file << out_str.str( ); // write to file
 
-    dump1.close( );
-    dump2.close( );
     out_file.close( );
 
     spdlog::info( "done, found {} relocs", reloc_data.size( ) );
